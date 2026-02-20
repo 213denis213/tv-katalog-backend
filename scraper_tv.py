a191456401a1483f139098e2b31be6645ec1da3e
213denis213 <denis.kaucic@gmail.com>
2026-02-20 11:15:23 +0100

    Fix typo in 'zivila' list for Slovenia

--- scraper_tv.py
+++ scraper_tv.py
@@ -12,8 +12,9 @@ from datetime import datetime
 
 NASTAVITVE_TRGOVIN = {
     "si": {
-        "zivila": ["hofer", "lidl", "spar", "mercator", "eurospin", "jager", "tus", "e-leclerc", "ahac", "bofrost"],
+        "zivila": ["hofer", "lidl", "spar", "mercator", "eurospin", "jager", "tus", "eleclerc", "ahac", "bofrost"],
         "tehnika": ["big-bang", "bela-plus", "conrad", "merkur"]
+        "dom-in-vrt": ["bauhaus", "harvey-norman", "dipo", "ikea", "inpos", "jysk", "kalia", "megadom", "obi", "momax", "rutar", "xxxl-lesnina", "zivex"]
     },
     "hr": {
         "prehrana": ["konzum", "lidl", "kaufland", "spar", "plodine", "tommy"],
