688d4a69b4fe99e4eb628b436a4064b0ca05bca0
213denis213 <denis.kaucic@gmail.com>
2026-02-20 20:42:02 +0100

    Update scraper_tv.py

--- scraper_tv.py
+++ scraper_tv.py
@@ -12,7 +12,11 @@ from datetime import datetime
 
 NASTAVITVE_TRGOVIN = {
     "si": {
-        "zivila": ["hofer", "lidl", "spar", "mercator", "eurospin", "jager", "tus", "eleclerc", "ahac", "bofrost"]
+        "zivila": ["hofer", "lidl", "spar", "mercator", "eurospin", "jager", "tus", "eleclerc", "ahac", "bofrost"],
+        "tehnika": ["big-bang", "bela-plus", "conrad", "merkur"],
+        "dom-in-vrt": ["bauhaus", "harvey-norman", "dipo", "ikea", "inpos", "jysk", "kalia", "megadom", "obi", "momax", "rutar", "xxxl-lesnina", "zivex"],
+        "drogerije-kozmetika": ["avon", "dm-drogerie-markt", "muller", "oriflame"],
+        "drugo": ["baby-center", "petrol", "tedi"],
     }
 }
 
