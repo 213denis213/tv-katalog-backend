3a91e88845277e4f728a4d8fcea02e18f2bee896
213denis213 <denis.kaucic@gmail.com>
2026-02-20 11:23:09 +0100

    Fix formatting issue in NASTAVITVE_TRGOVIN dictionary

--- scraper_tv.py
+++ scraper_tv.py
@@ -13,7 +13,7 @@ from datetime import datetime
 NASTAVITVE_TRGOVIN = {
     "si": {
         "zivila": ["hofer", "lidl", "spar", "mercator", "eurospin", "jager", "tus", "eleclerc", "ahac", "bofrost"],
-        "tehnika": ["big-bang", "bela-plus", "conrad", "merkur"]
+        "tehnika": ["big-bang", "bela-plus", "conrad", "merkur"],
         "dom-in-vrt": ["bauhaus", "harvey-norman", "dipo", "ikea", "inpos", "jysk", "kalia", "megadom", "obi", "momax", "rutar", "xxxl-lesnina", "zivex"]
     },
     "hr": {
